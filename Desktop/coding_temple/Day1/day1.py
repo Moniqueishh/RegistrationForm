2b4f80e49da0e362498d5a6e7c57c74dba6e298b
Monique Vargas <moniquevargas@Moniques-MacBook-Pro.local>
2023-02-20 19:59:08 -0600

    From Monique

---
+++ Desktop/coding_temple/Day1/day1.py
@@ -0,0 +1,16 @@
+#HOW TO EDIT SOMEONES CODE******
+
+#git clone (url)
+#git add .
+#git commit -m "notes"
+#git push origin main 
+#go back to Github and it will show your version 
+#go to your file/change 
+#Create a "Create Pull request" 
+#Can leave a comment 
+#"Create pull request"
+#Other person has to confirm pull request 
+
+#Other person***
+#refresh page where repo is
+#"Confirm merge"
